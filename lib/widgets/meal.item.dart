503c8596fa4ed0636a12e597293f9e9383ec08fe
Bilal Durmuş <bilaldurmus08@gmail.com>
2024-04-13 23:19:36 +0300

    meal.item class added

---
+++ lib/widgets/meal.item.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'package:meals_app/models/meal.dart';
+
+class MealItem extends StatelessWidget{
+  MealItem({super.key, required this.meal});
+
+  Meal meal; 
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('ads');
+  }
+}
